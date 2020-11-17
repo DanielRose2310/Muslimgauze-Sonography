0a9380a257d05a51a9ac63b761be84c3d10a305e
Daniel Rosenberg <dddrrr84@gmail.com>
2020-11-18 00:02:37 +0200

    pretty

--- public/javascripts/apiGets.js
+++ public/javascripts/apiGets.js
@@ -1,5 +1,5 @@
 export async function getRelData(_ref) {
-    _ref = _ref.replace(/[\[\]\:']+/g, '')
+    _ref = _ref.replace(/[\[\]\:]+/g, '')
     
         if (_ref.split(" ").length>2){_ref=_ref.split(" ").splice(0,2).join(" ")}
         
