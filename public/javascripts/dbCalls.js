bf2860e6130bc6301743529fbc4ac3c2094021f6
Daniel Rosenberg <dddrrr84@gmail.com>
2020-11-18 16:47:58 +0200

    stuff

--- public/javascripts/dbCalls.js
+++ public/javascripts/dbCalls.js
@@ -15,12 +15,13 @@ export const getData = () => {
 };
 
 export async function getRelData(_ref) {
-	_ref = _ref.replace(/[\[\]\:]+/g, "");
+	_ref = _ref.replace(/[\[\]\/\:]+/g, " ");
 
 	if (_ref.split(" ").length > 2) {
 		_ref = _ref.split(" ").splice(0, 2).join(" ");
 	}
 	let result;
+	console.log(_ref)
 	try {
 		const res = await axios.get(
 			`https://muslimgauze-database.herokuapp.com/albums/cataloguesearch/${_ref}/`,
