9414ffd83e8b3e201b3a71ad4b8a854fda19e8eb
Daniel Rosenberg <dddrrr84@gmail.com>
2020-11-19 18:58:13 +0200

    stuff

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors_proxy = require('cors-anywhere');
 
 const mongodb = require('./dbs_connected/mongodb')
 const tracksR = require('./routes/tracks');
