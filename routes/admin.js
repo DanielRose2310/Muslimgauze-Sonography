3bfd302d79809f693ae09715a948c12ed5fdce45
Daniel Rosenberg <dddrrr84@gmail.com>
2020-12-02 22:35:18 +0200

    admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const authToken = require("../middleware/auth");
 const {
     userModel,
     validUser,
