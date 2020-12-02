06e02bac48b127c3a4f498b38521d52180d08fe8
Daniel Rosenberg <dddrrr84@gmail.com>
2020-12-02 21:54:37 +0200

    admin

--- routes/admin.js
+++ routes/admin.js
@@ -4,11 +4,13 @@ router.all('/', (req,res, next)=> {
     });
     // or whatever
 });
-router.use('/media',express.static(path.join(__dirname, 'media')));
+//router.use('/media',express.static(path.join(__dirname, 'media')));
 router.all('/', (req,res, next)=> {
     res.status(403).send({
        message: 'Access Forbidden'
     });
     // or whatever
 });
-router.use('/media',express.static(path.join(__dirname, 'media')));
+//router.use('/media',express.static(path.join(__dirname, 'media')));
+
+module.exports = router;
