4179c5fe82060113aed102c30c440c288fd18f94
Daniel Rosenberg <dddrrr84@gmail.com>
2020-12-04 21:49:24 +0200

    admin

--- routes/admintrack.js
+++ routes/admintrack.js
@@ -15,8 +15,8 @@ router.get("/", authToken, async (req, res) => {
     
     let valid = validUser(req.body);
     if (!valid.error) {
-        res.send(process.cwd())
-        res.sendFile(path.join(process.cwd() + '/../admin/addtrack.html'));
+        //res.send(process.cwd())
+        res.sendFile(path.join(process.cwd() + '/admin/addtrack.html'));
 
     } else {
         res.status(403).send({
