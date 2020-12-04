163c36c9f50a65cf2ce51b3693cc59b7500bb64c
Daniel Rosenberg <dddrrr84@gmail.com>
2020-12-04 22:14:52 +0200

    admin

--- routes/adminalbum.js
+++ routes/adminalbum.js
@@ -15,8 +15,8 @@ router.get("/", authToken, async (req, res) => {
     
     let valid = validUser(req.body);
     if (!valid.error) {
-        //res.send(process.cwd())
-        res.sendFile(path.join(process.cwd() + './admin/addalbum.html'));
+        res.send(process.cwd())
+        res.sendFile(path.join(process.cwd() + '/public/admin/addAlbum.html'));
 
     } else {
         res.status(403).send({
